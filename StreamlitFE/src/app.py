aeeec82587b0b7b5d90eaedf969e770a9a25f7cd
LEE JINSU <bluesea9519@gmail.com>
2024-08-01 00:40:17 +0900

    UPDATE: update files

--- StreamlitFE/src/app.py
+++ StreamlitFE/src/app.py
@@ -1,32 +1,25 @@
 import streamlit as st
 from background import set_background
 from pubgstats import PUBGStatsApp
+from main import page1,page2
 
-def main():
-    # 이미지 파일 경로 설정
-    image_path = '../static/images/battleground1.png'
-    # 배경 이미지 설정 함수 호출
-    set_background(image_path)
-    # 상단에 배경 이미지를 설정한 헤더 추가
-    st.markdown('<div class="title">Whitezone Analysis Report</div>', unsafe_allow_html=True)
-    st.markdown('<div class="content">Battle Anywhere</div>', unsafe_allow_html=True)
-    st.markdown('</div>', unsafe_allow_html=True)
+#세션 상태 초기화
+if 'page' not in st.session_state:
+    st.session_state.page = 'page1'
+
+# 페이지 전환 함수
+def navigate_to(page):
+    st.session_state.page = page
 
 if __name__ == '__main__':
     # 이미지 파일 경로 설정
     image_path = '../static/images/battleground1.png'
     # 배경 이미지 설정 함수 호출
     set_background(image_path)
-    # 상단에 배경 이미지를 설정한 헤더 추가
-    st.markdown('<div class="centered">SSB.GG</div>', unsafe_allow_html=True)
-    # 페이지 여백을 설정하는 div 시작
-    with st.container():
-        st.markdown('<div class="custom-container">', unsafe_allow_html=True)
-        col1, col2 = st.columns([2, 5])
-        with col1:
-            platform = st.selectbox(" ", ["kakao", "steam"])
-        with col2:
-            user_input = st.text_input(" ", placeholder="닉네임을 입력하세요")
-        st.markdown('</div>', unsafe_allow_html=True)
+    # 현재 페이지에 따라 다른 UI를 표시합니다.
+    if st.session_state.page == 'page1':
+        page1(navigate_to)
+    elif st.session_state.page == 'page2':
+        page2(navigate_to)
 
 
--- StreamlitFE/src/main.py
+++ StreamlitFE/src/main.py
@@ -1,17 +1,33 @@
 import streamlit as st
-import base64
-from background import set_background
-st.set_page_config(layout="wide") # 화면 구성 wide
 
-def main():
-    # 이미지 파일 경로 설정
-    image_path = '../static/images/battleground1.png'
-    # 배경 이미지 설정 함수 호출
-    set_background(image_path)
+def page1(navigate_to):
     # 상단에 배경 이미지를 설정한 헤더 추가
-    st.markdown('<div class="title">Whitezone Analysis Report</div>', unsafe_allow_html=True)
-    st.markdown('<div class="content">Battle Anywhere</div>', unsafe_allow_html=True)
-    st.markdown('</div>', unsafe_allow_html=True)
+    st.markdown('<div class="centered">SSB.GG</div>', unsafe_allow_html=True)
+    # 페이지 여백을 설정하는 div 시작
+    with st.container():
+        st.markdown('<div class="custom-container">', unsafe_allow_html=True)
+        col1, col2 = st.columns([2, 5])
+        with col1:
+            platform = st.selectbox(" ", ["kakao", "steam"], key="platform")
+        with col2:
+            # user_input = st.text_input("",placeholder="닉네임을 입력하세요", on_change=navigate_to, args=("page2"))
+            # if user_input:
+            #     st.session_state.user_input = user_input
+            #     navigate_to("page2")
+            user_input = st.text_input("닉네임 입력", placeholder="닉네임을 입력하세요", key="user_input")
+            if st.button("Submit"):
+                if user_input:
+                    st.session_state.user_input = user_input  # 유저 입력값을 세션 상태에 저장
+                    navigate_to("page2")  # 페이지 2로 이동
+        st.markdown('</div>', unsafe_allow_html=True)
+def page2(navigate_to):
+    st.title("Page 2")
+    st.write("This is the second page.")
+    st.write(f"플랫폼: {st.session_state.get('platform', 'N/A')}")
+    st.write(f"닉네임: {st.session_state.get('user_input', 'N/A')}")
+    if st.button("Back to Page 1"):
+        navigate_to('page1')
+
 
 
 
